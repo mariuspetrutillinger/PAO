e1e5200e875c53842aff688e355ae2aaa2f0314d
Marius Petru Tillinger <marius.tillinger@gmail.com>
2023-03-31 15:47:32 +0300

    sa incepem

---
+++ Hotel_Management/src/Clients.java
@@ -0,0 +1,56 @@
+public class Clients {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private int day;
+    private int month;
+    private int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ Hotel_Management/src/Resorts.java
@@ -0,0 +1,29 @@
+public class Resorts {
+    private int id;
+    private String name;
+    private String location;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
